a6396f794366d69e62fc60a426fd7d672781f6cd
Vinci-da-Game <11582145@student.uts.edu.au>
2018-08-23 04:38:59 +1000

    refractor: add sass and reset project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+import 'jquery/dist/jquery.min.js';
+import 'normalize.css';
+import './style/index.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './js/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootStore = createStore(reducers, applyMiddleware(thunk));
+const RootContainer = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={rootStore}>
+        <App />
+    </Provider>,
+    RootContainer
+);
 registerServiceWorker();
