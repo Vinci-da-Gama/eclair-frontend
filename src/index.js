733fd715d61ab3b19ded9a5a5e111de40d9a43c8
Vinci-da-Game <11582145@student.uts.edu.au>
2018-08-23 05:00:40 +1000

    doc: move App.js to components folder --> start mapping list

--- src/index.js
+++ src/index.js
@@ -6,14 +6,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './App';
-// import thunk from 'redux-thunk';
-// import { createStore, applyMiddleware } from 'redux';
-// import reducers from './js/reducers';
+import App from './js/components/App';
 import rootStore from './js/store/store-config';
 import registerServiceWorker from './registerServiceWorker';
 
-// const rootStore = createStore(reducers, applyMiddleware(thunk));
 const RootContainer = document.getElementById('root');
 
 ReactDOM.render(
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-// import logo from './img/logo.svg';
-import ListContainer from './js/containers';
+import ListContainer from '../containers';
 
 class App extends Component {
 	render() {
