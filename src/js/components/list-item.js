f4ee8d524a9a72db9b6b7d28272dd80f09c7494d
Vinci-da-Game <11582145@student.uts.edu.au>
2018-08-23 06:09:34 +1000

    style: style the list

--- src/js/components/list-item.js
+++ src/js/components/list-item.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 export const itemComponent = (item, idx) => {
     return (
-        <div key={idx}>
-            {item.id}
+        <div className={`mb-2 p-0 alert h-77px border ${item.completed ? 'alert-light' : 'alert-danger border-danger'}`} role="alert" key={idx}>
+            <div className="row m-0">
+                <div className="col-3 p-2 h-77px">
+                    <p className={`p-0 m-0 text-center fz-id font-weight-bold rounded text-white ${item.completed ? 'bg-green': 'bg-red'}`}>
+                        {item.id}
+                    </p>
+                </div>
+                <div className="col-9 px-2 py-3">
+                    <div className={`text-uppercase fz-title ${item.completed ? 'text-green' : 'text-red'}`}>
+                        Title
+                    </div>
+                    <div className="text-dark text-truncate">
+                        {item.title}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
